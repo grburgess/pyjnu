2b5c4a621db6e8c3179df139b8a0deca479e4224
ga78fed <stephan.meighen-berger@tum.de>
2018-09-18 17:12:17 +0200

    Minor changes

--- py_core/example/example.py
+++ py_core/example/example.py
@@ -1,6 +1,6 @@
 
 import sys
-sys.path.append("/Users/theoglauch/Documents/PhD/Pyjnu-ECP/py_core/")
+sys.path.append("/Users/steph/Documents/PhD/pyjnu/py_core/")
 from pyjnu import PyRun
 from constants import phys_const
 import time
@@ -16,6 +16,6 @@ PYJNU = PyRun(
 )
 
 t1 = time.time()
-print('Time for intialization {} s'.format(t1-t0))
+print('Time for intialization %.2e s' % (t1-t0))
 PYJNU.solve_steady()
-print('Time for finding steady state solution {} s'.format(time.time()-t1))
+print('Time for finding steady state solution %.2e s' % (time.time()-t1))
